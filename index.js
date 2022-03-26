ecc10634c42a232f8eb260a0b8b78dde9eb366a3
Trafael Lewis <TrafLewis@gmail.com>
2022-03-26 13:38:15 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solutions in this file
+const friends = ["Guadalupe", "Ollie", "Aki"];
+
+function writeCards(friends) {
+
+  const cards = []
+
+  for (let i = 0; i < friends.length; i++) {
+    cards.push(`Thank you, ${friends[i]}, for the wonderful surprise gift!`);
+  }
+
+return cards;
+
+}
+
+
+
+function countDown() {
+  let timer = 10;
+  while (timer >= 0) {
+    console.log(timer--)
+  }
+}
